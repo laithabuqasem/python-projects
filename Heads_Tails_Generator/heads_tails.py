cf0e6b80ed493130c46b66dc934d71263bfa709a
laithabuqasem <108104314+laithabuqasem@users.noreply.github.com>
2023-05-03 21:59:34 -0500

    created if statements for if 1 was the random number chosen

--- Heads_Tails_Generator/heads_tails.py
+++ Heads_Tails_Generator/heads_tails.py
@@ -2,3 +2,10 @@ import random #imports module named: random
 
 choice = input("Pick Heads or Tails: ").lower() #asks user to pick heads or tails
 flip = random.randint(0, 1) #generates random number to pick heads or tails
+
+if flip == 1: #if heads
+    print("It was heads!")
+    if choice == "heads":
+        print(f"Great job! It was {choice} you got it right!")
+    elif choice == "tails":
+        print(f"You got it wrong! It's not {choice}.")
