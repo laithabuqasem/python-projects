2273dc3871c80375979b2c003757e727c6d781d7
laithabuqasem <108104314+laithabuqasem@users.noreply.github.com>
2023-05-03 21:58:42 -0500

    created 2 variables, choice: inputs users pick fo heads or tails. flip: generates random number, either 0 or 1

--- Heads_Tails_Generator/heads_tails.py
+++ Heads_Tails_Generator/heads_tails.py
@@ -1,1 +1,4 @@
 import random #imports module named: random
+
+choice = input("Pick Heads or Tails: ").lower() #asks user to pick heads or tails
+flip = random.randint(0, 1) #generates random number to pick heads or tails
