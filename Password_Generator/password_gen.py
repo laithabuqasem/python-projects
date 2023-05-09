d44cfda92c3c828920105882ecbe9bee048f21d6
laithabuqasem <108104314+laithabuqasem@users.noreply.github.com>
2023-05-09 01:03:36 -0500

    used random and shuffle to create random password

--- Password_Generator/password_gen.py
+++ Password_Generator/password_gen.py
@@ -7,3 +7,21 @@ print("Welcome to the PyPassword Generator!")
 nr_letters = int(input("How many letters would you like in your password?\n"))
 nr_symbols = int(input(f"How many symbols would you like?\n"))
 nr_numbers = int(input(f"How many numbers would you like?\n"))
+
+password_list = []
+
+for char in range(1, nr_letters + 1):
+  password_list.append(random.choice(letters))
+
+for char in range(1, nr_symbols + 1):
+  password_list += random.choice(symbols)
+
+for char in range(1, nr_numbers + 1):
+  password_list += random.choice(numbers)
+
+random.shuffle(password_list)
+password = ""
+for char in password_list:
+  password += char
+
+print(f"Your password is: {password}")
