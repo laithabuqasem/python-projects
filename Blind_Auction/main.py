18954c3761bb1b73586f5dffd63467f268556777
laithabuqasem <108104314+laithabuqasem@users.noreply.github.com>
2023-05-19 22:38:01 -0500

    created while loop and dict to keep bids going until someone types no

--- Blind_Auction/main.py
+++ Blind_Auction/main.py
@@ -1,3 +1,19 @@
+import os
 import art
 print(art.logo)
 
+place_holder = "NA"
+
+another_person = True
+while another_person:
+    name = input("Enter your name: ")
+    bid_price = int(input("Enter bid price: "))
+
+    person = {name: bid_price}
+
+    result = input("Are there other bidders?").lower()
+    if result == "no":
+        another_person = False
+        print(f"The highest bidder is: {place_holder} with ${place_holder} ")
+    elif result == "yes":
+        os.system("cls")
