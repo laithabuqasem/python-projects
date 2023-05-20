9d8363c76e6f728ee463dcc7dac770deffc5c422
laithabuqasem <108104314+laithabuqasem@users.noreply.github.com>
2023-05-19 22:49:42 -0500

    finished working code for blind auction

--- Blind_Auction/main.py
+++ Blind_Auction/main.py
@@ -3,17 +3,20 @@ import art
 print(art.logo)
 
 place_holder = "NA"
+person = {}
 
 another_person = True
 while another_person:
     name = input("Enter your name: ")
     bid_price = int(input("Enter bid price: "))
 
-    person = {name: bid_price}
+    person[name] = bid_price
 
     result = input("Are there other bidders?").lower()
     if result == "no":
         another_person = False
-        print(f"The highest bidder is: {place_holder} with ${place_holder} ")
+        max_value = max(person.values())
+        max_key = max(person, key = person.get)
+        print(f"The highest bidder is {max_key} with ${max_value} ")
     elif result == "yes":
         os.system("cls")
