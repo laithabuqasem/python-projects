44fa0408221a473b7f54f27e5b75e0133bd0fe12
laithabuqasem <108104314+laithabuqasem@users.noreply.github.com>
2023-05-29 22:41:02 -0500

    created functions for mathematical operations and added documentation

---
+++ Calculator_using_Functions/main.py
@@ -0,0 +1,22 @@
+
+#Calculator
+
+#Add
+def add(n1,n2):
+    """Adds the two given values: n1, n2 and returns the value"""
+    return n1 + n2
+
+#Subtract
+def subtract(n1, n2):
+    """Subtracts the two given values: n1, n2 and returns the value"""
+    return n1 - n2
+
+#Multiply
+def multiply(n1, n2):
+    """Multiplies the two given values: n1, n2 and returns the value"""
+    return n1 * n2
+
+#Divide
+def divide(n1, n2):
+    """Divides the two given values: n1, n2 and returns the value"""
+    return n1 / n2
