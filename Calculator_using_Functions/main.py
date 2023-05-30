c4ab8f53c3f2a1b08639bf4524cb9be91e7fc67c
laithabuqasem <108104314+laithabuqasem@users.noreply.github.com>
2023-05-29 22:50:33 -0500

    made user experience better

--- Calculator_using_Functions/main.py
+++ Calculator_using_Functions/main.py
@@ -27,3 +27,16 @@ operations = {
     "*": multiply,
     "/": divide
 }
+
+num1 = int(input("What's the first number?: "))
+
+#displays the operations for the user to choose from
+for symbol in operations:
+    print(symbol)
+operation_symbol = input("Pick an operation from the line above: ")
+
+num2 = int(input("What's the second number?: "))
+
+calculation_function = operations[operation_symbol]
+answer = calculation_function(num1, num2)
+print(f"{num1} {operation_symbol} {num2} = {answer}")
