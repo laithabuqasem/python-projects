284cb4f22d9b98976160da0f231902e988b7db34
laithabuqasem <108104314+laithabuqasem@users.noreply.github.com>
2023-06-04 23:23:13 -0500

    created randomization of variables and had to print spaces instead of cls os command because of pycharm

--- Blackjack_Game/main.py
+++ Blackjack_Game/main.py
@@ -1,1 +1,26 @@
-play = input("Do you want to play a game of Blackjack? Type 'y' or 'n': ")
+import random
+# import os
+
+play = input("Do you want to play a game of Blackjack? Type 'y' or 'n': ").lower()
+
+if play == "y":
+    # os.system('cls')
+    print('\n' * 10)
+
+    c1 = random.randint(1, 12)
+    c2 = random.randint(1, 12)
+    cc1 = random.randint(1, 12)
+
+    print(f"Your cards: [{c1}, {c2}]")
+    print(f"Computer's first card: {cc1}")
+    next_card = input("Type 'y' to get another card, type 'n' to pass: ")
+elif play == "n":
+    print("Goodbye!")
+else:
+    print("Invalid input!")
+
+"""
+    Your cards: [9, 10]
+    Computer's first card:
+    Type 'y' to get another card, type 'n' to pass: 
+"""
